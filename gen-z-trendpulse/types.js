e5a6946153c5b023bdb223121eecaf1475845da9
Natalija Janz <ntjanz@gmail.com>
2025-11-27 04:23:34 +0000

    Add Google-data-trends.ipynb; remove gen-z-trendpulse (exclude TrendPulse and gemini)

--- gen-z-trendpulse/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Category = void 0;
-var Category;
-(function (Category) {
-    Category["FASHION"] = "Fashion";
-    Category["TECH"] = "Tech";
-    Category["LIFESTYLE"] = "Lifestyle";
-})(Category || (exports.Category = Category = {}));
